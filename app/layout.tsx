4659e5c8ca9655f464ea4b7386e495d7d8ba379b
Bahram Movlanov <bmovlanov@gmail.com>
2023-10-22 22:24:19 -0400

    add seo metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'JS Mastery',
   description: 'JS Mastery Resources',
+  other: {
+    'theme-color': '#0c1319',
+    'color-scheme': 'dark only',
+    'twitter:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.png',
+    'twitter:card': 'summary_large_image',
+    'og:url': 'bahram.dev',
+    'og:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.png',
+    'og:type': 'website',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
